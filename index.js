e5f0c6622103e7548cdc25ea37b82693459a659e
Nattasit3674 <s6406021611069@email.kmutnb.ac.th>
2024-02-12 11:20:22 +0700

    .

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-//const base_url = "http://10.104.16.40:3000";
+const base_url = "http://10.104.16.40:3000";
 
-const base_url = "http://env-3392176.proen.app.ruk-com.cloud/";
+//const base_url = "http://env-3392176.proen.app.ruk-com.cloud/";
 
 // Set the template engine
 app.set("views", path.join(__dirname, "/public/views"));
