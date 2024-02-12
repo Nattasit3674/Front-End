8f22c243161070fcf4fa6bc97119f39da165439a
Nattasit3674 <s6406021611069@email.kmutnb.ac.th>
2024-02-12 10:36:01 +0700

    index

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
 
 const express = require("express");
 const axios = require("axios");
-const app = express();
+const path = require("path");
 var bodyParser = require("body-parser");
+const app = express();
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "http://10.104.16.40:3000";
+//const base_url = "http://10.104.16.40:3000";
 
-//const base_url = "http://node41091-noderest.proen.app.ruk-com.cloud";
+const base_url = "http://env-3392176.proen.app.ruk-com.cloud/";
 
 // Set the template engine
-//app.set("views", path.join(__dirname, "/public/views"));
+app.set("views", path.join(__dirname, "/public/views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
